c3b307818d8de10443daeca18e2969bf1f384aec
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-06-17 21:28:36 +0530

    [akhila]Adding:Creatinglist

---
+++ StacksAndQueues/StacksAndQueues/Node.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues
+{
+    /// <summary>
+    /// Implementing the stack by using linkedlist
+    /// </summary>
+    public class Node
+    {
+        public int data;
+        public Node next;
+        private Node top;
+
+        public Node()
+        {
+            this.top = null;
+        }
+        /// <summary>
+        /// push the data to top of the stack
+        /// </summary>
+        /// <param name="data"></param>       
+        public void Push(int data)
+        {
+            Node node = new Node();
+            node.data = data;
+
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("element pushed to stack:" + data);
+        }
+        /// <summary>
+        /// displays the stacks list
+        /// </summary>
+        public void Display()
+        {
+            Node temp = this.top;
+            while(temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+    
+}
---
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Node node = new Node();
+            node.Push(56);
+            node.Push(30);
+            node.Push(70);
+            node.Display();
+        }
+    }
+}
